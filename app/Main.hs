57ff45ec69e04ee31038f1ba279b4959b4a8f315
Eli Most <mosteliml@gmail.com>
2018-01-28 15:47:04 -0800

    Added assignment-1 functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,42 @@
 module Main where
 
-import Lib
+import Options.Applicative
+import Data.Semigroup ((<>))
 
 main :: IO ()
-main = someFunc
+main = greetBasic =<< execParser opts
+    where
+        opts = info (basic <**> helper)
+            ( fullDesc
+            <> progDesc "Compile nothing at all!"
+            <> header "aardvark - a fuzzy animal compiler" )
+
+data Arg = Arg
+    { isLength :: Bool
+    , otherArgs :: [String] }
+
+greetBasic :: Basic -> IO ()
+greetBasic (Basic b arguments) 
+    | b = printNums arguments
+    | otherwise = printArgs arguments
+    where
+        printArgs :: [String] -> IO () 
+        printArgs [] = return ()
+        printArgs (a:ls)  = (putStrLn a) >> printArgs ls
+        printNums :: [String] -> IO ()
+        printNums [] = return ()
+        printNums (a:ls) = (putStrLn (show (length a))) >> printNums ls
+
+data Basic = Basic
+    { reportLength :: Bool
+    , args :: [String] }
+
+basic :: Parser Basic
+basic = Basic
+    <$> switch
+        ( long "length" 
+        <> short 'l'
+        <> help "Prints the lengths of the arguments")
+    <*> many
+        (argument str (metavar "argument")) 
+    
