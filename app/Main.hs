5a3082d1f1a29fd9791d6e3854c2f5582e528651
Eli Most <mosteliml@gmail.com>
2018-03-13 07:57:47 -0700

    Added basic environment evaluation steps for reference operators

--- app/Main.hs
+++ app/Main.hs
@@ -29,9 +29,9 @@ greetBasic (Basic lex par read step prog)
         let e = P.parser $ L.alexScanTokens input in
             let t = TC.typecheck' e in
                 case t of
-                    _ -> E.evalFinal $ E.deepEval $ P.parser $ L.alexScanTokens input
-                    G.YBool -> E.evalFinal $ E.deepEval $ P.parser $ L.alexScanTokens input
-    | step = E.showExecutionSteps $ parsed
+                    _ -> E.evalFinal $ E.deepEval' [] $ P.parser $ L.alexScanTokens input
+                    G.YBool -> E.evalFinal $ E.deepEval'[] $ P.parser $ L.alexScanTokens input
+    | step = E.showExecutionSteps [] $ parsed
     | lex = putStrLn $ show $ lexed
     | par = if null lexed
         then putStrLn "No tokens were parsed. Did you pass an empty file?" 
@@ -41,12 +41,11 @@ greetBasic (Basic lex par read step prog)
         else
             let t = TC.typecheck' parsed in
                 case t of 
-                    _ -> E.evalFinal $ E.deepEval $ parsed
-                    G.YBool -> E.evalFinal $ E.deepEval $ parsed
+                    _ -> E.evalFinal $ E.deepEval' [] $ parsed
+                    G.YBool -> E.evalFinal $ E.deepEval' [] $ parsed
     where
         lexed = L.alexScanTokens prog
-        parsed = P.parser $ lexed
-        
+        parsed = P.parser $ lexed   
 
 data Basic = Basic
     { useLexer  :: Bool
--- src/Eval.hs
+++ src/Eval.hs
@@ -5,100 +5,132 @@ module Eval where
 import Grammar
 import Control.Monad.Reader
 import qualified Data.Map.Strict as Map
+import Data.List
 
-eval :: Exp -> Exp
-eval (EBinop p op e1 e2) = case (e1, e2) of 
+eval :: EvalEnv -> Exp -> (EvalEnv, Exp)
+eval env (EBinop p op e1 e2) = case (e1, e2) of 
     (EVal v1, EVal v2) -> case (v1, v2) of
         ((EInt _ n1), r2) -> case r2 of
-            (EInt _ n2) -> EVal $ (EInt p) $ (if op == Div then div else (binopIntFunc op)) n1 n2
-            (EFloat _ n2) -> EVal $ (EFloat p) $ (binopFracFunc op) (fromIntegral n1) n2
+            (EInt _ n2) -> let v = EVal $ (EInt p) $ (if op == Div then div else (binopIntFunc op)) n1 n2 in (env, v)
+            (EFloat _ n2) -> let v = EVal $ (EFloat p) $ (binopFracFunc op) (fromIntegral n1) n2 in (env, v)
             _           -> error $ "Type mismatch at column " ++ show (col p) ++ ", line " ++ show (line p)
-        ((EFloat _ n1), (EFloat _ n2)) -> EVal $ (EFloat p) $ (binopFracFunc op) n1 n2
+        ((EFloat _ n1), (EFloat _ n2)) -> let v = EVal $ (EFloat p) $ (binopFracFunc op) n1 n2 in (env, v)
         ((EFloat _ n1), r2) -> case r2 of
-            (EInt _ n2) -> EVal $ (EFloat p) $ (binopFracFunc op) n1 (fromIntegral n2)
-            (ENaN _) -> EVal $ ENaN p
+            (EInt _ n2) -> let v = EVal $ (EFloat p) $ (binopFracFunc op) n1 (fromIntegral n2) in (env, v)
+            (ENaN _) -> (env, EVal $ ENaN p)
             _        -> error $ "Type mismatch at column " ++ show (col p) ++ " , line " ++ show (line p)
         ((ENaN _), r2) -> case r2 of
-            (ENaN _) -> EVal $ ENaN p
-            (EFloat _ _) -> EVal $ ENaN p
+            (ENaN _) -> (env, EVal $ ENaN p)
+            (EFloat _ _) -> (env, EVal $ ENaN p)
         (_, _) -> error $ (show op) ++ " invalid types error at column " ++ show (col p) ++ " , line " ++ show (line p)
     (var1@(EVar p2 str), e3) -> case e3 of
-        var2@(EVar _ _) -> (binopToConstructor p op) var1 var2
-        _ -> eval $ (binopToConstructor p op) (EVar p2 str) (eval e3)
-    (e3, var1@(EVar _ _)) -> eval $ (binopToConstructor p op) (eval e3) var1
-    (EVal _, _) -> binopToConstructor p op e1 (eval e2)
-    (_, _) -> binopToConstructor p op (eval e1) e2
+        var2@(EVar _ _) -> (env, (binopToConstructor p op) var1 var2)
+        _ -> case eval env e3 of 
+            (env2, newE) -> eval env2 $ (binopToConstructor p op) (EVar p2 str) newE
+    (e3, var1@(EVar _ _)) -> case eval env e3 of 
+        (env2, newE3) -> eval env2 $ (binopToConstructor p op) newE3 var1
+    (EVal _, _) -> case eval env e2 of 
+        (env2, newE2) -> (env2, binopToConstructor p op e1 newE2)
+    (_, _) -> case eval env e1 of 
+        (env2, newE1) -> (env2, binopToConstructor p op newE1 e2)
 
-eval (EIf p e1 e2 e3) = case e1 of
+eval env (EIf p e1 e2 e3) = case e1 of
     (EVal v1) -> case v1 of
-        (EBool _ True) -> e2
-        (EBool _ False) -> e3
+        (EBool _ True) -> (env, e2)
+        (EBool _ False) -> (env, e3)
         _ -> error $ "Type mismatch at column " ++ show (col p) ++ ", line " ++ show (line p)
     (ELessEqThan _ r1 r2) -> case (r1, r2) of
         (EVal v1, EVal v2) -> case (v1, v2) of 
-            ((EInt _ n1), (EInt _ n2)) -> if n1 <= n2 then e2 else e3
-            ((EInt _ n1), (EFloat _ n2)) -> if (fromIntegral n1) <= n2 then e2 else e3
-            ((EFloat _ n1), (EInt _ n2)) -> if n1 <= (fromIntegral n2) then e2 else e3
-            ((EFloat _ n1), (EFloat _ n2)) -> if n1 <= n2 then e2 else e3
+            ((EInt _ n1), (EInt _ n2)) -> if n1 <= n2 then (env, e2) else (env, e3)
+            ((EInt _ n1), (EFloat _ n2)) -> if (fromIntegral n1) <= n2 then (env, e2) else (env, e3)
+            ((EFloat _ n1), (EInt _ n2)) -> if n1 <= (fromIntegral n2) then (env, e2) else (env, e3)
+            ((EFloat _ n1), (EFloat _ n2)) -> if n1 <= n2 then (env, e2) else (env, e3)
             _ -> error $ "Type mismatch at column " ++ show (col p) ++ ", line " ++ show (line p)
     _ -> error $ "Type mismatch at column " ++ show (col p) ++ ", line " ++ show (line p)
 
-eval var@(EVar _ _) = var 
+eval env var@(EVar _ _) = (env, var) 
 
-eval (ELet _ s (EVal v) e2 _) = subst v s e2 
-eval (ELet p s e1 e2 t) = ELet p s (eval e1) e2 t
+eval env (ELet _ s (EVal v) e2 _) = (env, subst v s e2)
+eval env (ELet p s e1 e2 t) = case eval env e1 of
+    (env2, exp2) -> (env2, ELet p s exp2 e2 t)
 
-eval (EApp p e1 e2) = case (e1, e2) of
+eval env (EApp p e1 e2) = case (e1, e2) of
     (EVal v1, EVal v2) -> case v1 of
-        (EFunc _ s funcExp _) -> subst v2 s funcExp
-        fix@(EFix _ funcName varName funcExp _) -> let varReplaced = subst v2 varName funcExp in subst fix funcName varReplaced
+        (EFunc _ s funcExp _) -> (env, subst v2 s funcExp)
+        fix@(EFix _ funcName varName funcExp _) -> let varReplaced = subst v2 varName funcExp 
+                                                   in (env, subst fix funcName varReplaced)
         _ -> error $ (ppPos p) ++ " Function application operator applied where first paramater is not a function"
     (EVal _, var) -> case var of 
         (EVar _ _) -> error $ (ppPos p) ++ " Application of function to unresolved variable" 
-        b@(EBinop _ _ _ _) -> EApp p e1 (eval b)
+        b@(EBinop _ _ _ _) -> case eval env b of 
+            (env2, exp2) -> (env2, EApp p e1 exp2)
         _ -> error $ (ppPos p) ++ " Application of function to expression which did not resolve to a value"
-    (_, _) -> EApp p (eval e1) (eval e2)
+    (_, _) -> case eval env e1 of
+        (env2, exp2) -> case eval env e2 of
+            (env3, exp3) -> (env2 ++ env3, EApp p exp2 exp3)
 
-eval (EFst _ e1) = eval e1 
-eval (ESnd _ e2) = eval e2
-eval (EEmpty p e1) = 
+eval env (EFst _ e1) = eval env e1 
+eval env (ESnd _ e2) = eval env e2
+eval env (EEmpty p e1) = 
     case e1 of 
-        (EVal (ENil _ _)) -> EVal $ EBool p True
-        _          -> EVal $ EBool p False 
-eval (EHead p e) = 
+        (EVal (ENil _ _)) -> (env, EVal $ EBool p True)
+        _          -> (env, EVal $ EBool p False)
+eval env (EHead p e) = 
     case e of 
-        (EVal (ECons p e1 e2)) -> eval e1 
+        (EVal (ECons p e1 e2)) -> eval env e1
         (EVal (ENil _ _)) -> error $ (ppPos p) ++ " Application of head on empty list"
         _ -> error $ (ppPos p) ++ " Application of head on non-list type"
-eval (ETail p e) = 
+eval env (ETail p e) = 
     case e of 
-        (EVal (ECons p e1 e2)) -> eval e2 
+        (EVal (ECons p e1 e2)) -> eval env e2 
         (EVal (ENil _ _)) -> error $ (ppPos p) ++ " Application of tail on empty list"
         _ -> error $ (ppPos p) ++ " Application of tail on non-list type"
+eval env (ERef p e) = 
+    case e of 
+        (EVal v) -> let ptr = length env in (env ++ [(ptr, v)], EPtr ptr)
+eval env (EAssignment p e1 e2) = 
+    case (e1, e2) of 
+        (EPtr n, (EVal newValue)) -> case lookup n env of 
+            Just _ -> let oldPair = (n, newValue) in 
+                let l = delete oldPair env in 
+                ((n, newValue):l, EVal $ EEvalUnit)
+            Nothing -> error $ (ppPos p) ++ " Reassignment to reference missing from environement"
+eval env (EBang p e) = 
+    case e of 
+        (EPtr n) -> case lookup n env of 
+            Just val -> (env, EVal $ val)
+            _        -> error $ (ppPos p) ++ " Dereference of variable not found in environment"
 
-eval e1 = e1
 
-deepEval :: Exp -> Exp
-deepEval v@(EVal _) = v
-deepEval e = deepEval (eval e)
+eval env e1 = eval env e1
 
-stepNEval :: Int -> Exp -> Exp
-stepNEval _ v@(EVal _) = v
-stepNEval 0 e = e
-stepNEval n e = stepNEval (n-1) (eval e)
+deepEval :: EvalEnv -> Exp -> (EvalEnv, Exp)
+deepEval env v@(EVal _) = (env, v)
+deepEval env e = case eval env e of 
+    (env2, e2) -> deepEval env2 e2
 
-showExecutionSteps :: Exp -> IO ()
-showExecutionSteps v@(EVal _) = putStrLn $ show v
-showExecutionSteps e = do
+deepEval' :: EvalEnv -> Exp -> Exp 
+deepEval' env e = snd $ deepEval env e 
+
+stepNEval :: EvalEnv -> Int -> Exp -> (EvalEnv, Exp)
+stepNEval env _ v@(EVal _) = (env, v)
+stepNEval env 0 e = (env, e)
+stepNEval env n e = case eval env e of
+    (env2, e2) -> stepNEval env2 (n-1) e2
+
+showExecutionSteps :: EvalEnv -> Exp -> IO ()
+showExecutionSteps env v@(EVal _) = putStrLn $ (show v) ++ "\nEnv: " ++ (show env)
+showExecutionSteps env e = do
     putStrLn $ show e 
-    e <- helper (pure e)
-    showExecutionSteps e
+    (env2, exp2) <- helper $ pure (env, e)
+    showExecutionSteps env2 exp2
     where
-        helper :: IO Exp -> IO Exp 
+        helper :: IO (EvalEnv, Exp) -> IO (EvalEnv, Exp) 
         helper ioe = do
-            exp <- ioe
-            putStrLn $ show exp
-            return $ eval exp
+            (env, exp) <- ioe
+            putStrLn $ (show exp) ++ "\nEnv: " ++ (show env)
+            case eval env exp of
+                (env2, exp2) -> return (env2, exp2)
 
 binopIntFunc :: Integral a => EOptype -> a -> a -> a
 binopIntFunc Add = (+)
